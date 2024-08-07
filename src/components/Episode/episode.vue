8545ca64d65bb011d33a6c35598b4b55aade000c
Diogo Vilas Boas <diogovilasboas98@gmail.com>
2024-08-07 22:04:37 +0200

    make the tv show page and episode page with tests

--- src/components/Episode/episode.vue
+++ src/components/Episode/episode.vue
@@ -1,3 +1,29 @@
+<script lang="ts" setup>
+import useTvShowStore from '../../stores/tvShowStore';
+import { useRouter, useRoute } from 'vue-router';
+
+const store = useTvShowStore()
+const router = useRouter()
+const route = useRoute()
+
+store.fetchTvEpisodes()
+if(!route?.params.season && route?.params.ep){
+    router.push('/')
+}
+
+</script>
 <template>
-    Episode
+    <div>
+        <h1 data-test="title" class="mb-5">{{ store.getEpisodeBySeasonEpNumber(+route.params.season, +route.params.ep )?.title }}</h1>
+        <PVImage data-test="coverImage" imageClass="my-5 mx-auto" :src="store.getEpisodeBySeasonEpNumber(+route.params.season, +route.params.ep )?.episodeCoverImageUrl" :alt="store.getEpisodeBySeasonEpNumber(+route.params.season, +route.params.ep )?.title" width="400" />
+        
+        <div data-test="description" class="description mx-auto mb-5 w-[70vw]" v-html="store.getEpisodeBySeasonEpNumber(+route.params.season, +route.params.ep )?.summary"/>
+
+        <PVButton class=" mx-auto mt-5" @click="router.go(-1)">
+            <i class="pi pi-arrow-left"></i>
+        </PVButton>
+    </div>
 </template>
+<style scoped>
+@import './episode.css';
+</style>
--- src/components/TvShow/tvShow.test.ts
+++ src/components/TvShow/tvShow.test.ts
@@ -1,11 +1,183 @@
-import { expect, it, describe} from 'vitest'
+import { expect, it, describe, beforeEach} from 'vitest'
 import { mount } from '@vue/test-utils'
 import TvShow from './tvShow.vue'
+import usetvShowStore from '../../stores/tvShowStore';
+
+import { setActivePinia, createPinia } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
+import PrimeVue from 'primevue/config';
+
+import PVImage from 'primevue/image'
+import PVTabs from 'primevue/tabs'
+import PVTab from 'primevue/tab'
+//import PVTabList from 'primevue/tablist'
+import PVDataView from 'primevue/dataview'
+import { nextTick } from 'vue';
+import router from '../../router/routes'
+
+
+const components = {
+    PVImage,
+    PVTab,
+    PVTabs,
+    PVDataView,
+    //PVTabList
+}
+
+const originalConsoleError = console.error;
+const jsDomCssError = "Error: Could not parse CSS stylesheet";
+console.error = (...params) => {
+  if (!params.find((p) => p.toString().includes(jsDomCssError))) {
+    originalConsoleError(...params);
+  }
+};
 
 describe('TV Show', () => {
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
+    
     it('Loads succefully', ()=>{
-        const wrapper = mount(TvShow)
+        const wrapper = mount(TvShow,{
+            global:{
+                components:components,
+                plugins:[
+                    createTestingPinia(),
+                    router,
+                    PrimeVue
+                ]
+            }
+        })
         
-        expect(wrapper.exists()).toBeTruthy()
+        expect(wrapper).toBeTruthy()
+    })
+
+    it('Has title', async ()=>{
+        const wrapper = mount(TvShow,{
+            global:{
+                components:components,
+                plugins: [
+                    createTestingPinia(),
+                    router,
+                    PrimeVue
+                ],
+            }
+        })
+        const store = usetvShowStore()
+        store.title= "Test Title"
+        const title = wrapper.get('[data-test="title"]')
+        await nextTick();
+        expect(title.text()).toBe(store.title)
+    }),
+
+    it('Has description', async ()=>{
+        const wrapper = mount(TvShow,{
+            global:{
+                components:components,
+                plugins: [
+                    createTestingPinia(),
+                    router,
+                    PrimeVue
+                ],
+            }
+        })
+        const store = usetvShowStore()
+        store.description= "Test description"
+        const description = wrapper.get('[data-test="description"]')
+        await nextTick();
+        expect(description.text()).toBe(store.description)
+    }),
+
+    it('Has cover image', async ()=>{
+        const wrapper = mount(TvShow,{
+            global:{
+                components:components,
+                plugins: [
+                    createTestingPinia(),
+                    router,
+                    PrimeVue
+                ],
+            }
+        })
+        const store = usetvShowStore()
+        store.coverImageUrl= "image.png"
+        const coverImage = wrapper.get('[data-test="coverImage"]')
+        await nextTick();
+        expect(coverImage.element.attributes.getNamedItem('src')?.value).toBe(store.coverImageUrl)
+    })
+
+    it('Has seasons', async()=>{
+        const wrapper = mount(TvShow,{
+            global:{
+                components:components,
+                plugins: [
+                    createTestingPinia(),
+                    router,
+                    PrimeVue
+                ],
+            }
+        })
+        const store = usetvShowStore()
+        store.episodeList= [{
+            title: 'test',
+            summary: 'test',
+            episodeCoverImageUrl: 'url',
+            season: 1,
+            number: 1
+        },
+        {
+            title: 'test',
+            summary: 'test',
+            episodeCoverImageUrl: 'url',
+            season: 1,
+            number: 1
+        },{
+            title: 'test',
+            summary: 'test',
+            episodeCoverImageUrl: 'url',
+            season: 2,
+            number: 1
+        }]
+        const seasonTabs = wrapper.get('[data-test="tabs"]')
+        await nextTick();
+        expect(seasonTabs.element.children.length).toBe(2)
+    }),
+    it('Has episodes', async()=>{
+        const wrapper = mount(TvShow,{
+            global:{
+                components:components,
+                plugins: [
+                    createTestingPinia(),
+                    router,
+                    PrimeVue
+                ],
+            }
+        })
+        const store = usetvShowStore()
+        store.episodeList= [{
+            title: 'test',
+            summary: 'test',
+            episodeCoverImageUrl: 'url',
+            season: 1,
+            number: 1
+        },
+        {
+            title: 'test',
+            summary: 'test',
+            episodeCoverImageUrl: 'url',
+            season: 1,
+            number: 1
+        },{
+            title: 'test',
+            summary: 'test',
+            episodeCoverImageUrl: 'url',
+            season: 2,
+            number: 1
+        }]
+        
+        await nextTick();
+        expect(store.getEpisodesBySeason(1)).toHaveLength(2)
     })
 })
+
+
